a360e399092421c5ad59ec8b32ef1a3741e3cfdd
diego-2106 <ez.mm.gg@gmail.com>
2023-08-10 09:10:45 -0300

    Registro

--- js/validacion.js
+++ js/validacion.js
@@ -6,5 +6,26 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-const nameInput = document.getElementById("nombre");
-const passwordInput = document.getElementById("password");
+const nameInput = document.querySelector("#nombre");
+const apellidoInput = document.querySelector("#apellido");
+const email = document.querySelector("#email");
+const password1Input = document.querySelector("#password1");
+const password2Input = document.querySelector("#password2");
+const checkboxInput = document.querySelector("#terminos")
+const btn = document.querySelector("#redBtn");
+
+
+function validarCampos() {
+    if(nameInput.value === '' || apellidoInput.value === '' || email.value === '' || password1Input.value === '' || password2Input.value === '' || checkboxInput.checked === false) {
+        showAlertError();
+    } if (!password1Input.value.length >= 6 || !password2Input.value.length >= 6) {
+        showAlertError()
+    } if (!password1Input.value === !password2Input.value) {
+        showAlertError()
+    } if (!checkboxInput.checked === true){
+        showAlertError()
+    }
+   else {
+    showAlertSuccess();
+   }
+}
